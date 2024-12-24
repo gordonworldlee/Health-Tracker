7b796bfd64253b92ca44675924b617f2675357e1
Gordon <gordonworldlee@gmail.com>
2024-12-24 15:27:47 -0600

    Initial Commit

---
+++ Health Tracker/Health_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_TrackerApp.swift
+//  Health Tracker
+//
+//  Created by Gordon Lee on 12/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Health_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
